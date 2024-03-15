df7b8efbfe194e2c56348ac1cec8656160977ef5
BoFo <brunofazio466@gmail.com>
2024-03-15 12:42:42 -0300

    componente card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,50 +14,10 @@ export default function Home() {
         <p className="text-2xl">Tienda de productos</p>
       </div>
       <div className="grid gap-16 pb-12 justify-around md:grid-cols-2">
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/imagen1.png"
-              alt="Picture of the author"
-              width={500}
-              height={500}
-            />
-            <h2 className="text-2xl font-bold">Productos</h2>
-            <p className="text-lg">Productos de alta calidad</p>
-            <Button>Ver productos</Button>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/imagen2.png"
-              alt="Picture of the author"
-              width={500}
-              height={500}
-            />
-            <h2 className="text-2xl font-bold">Contacto</h2>
-            <p className="text-lg">Contactanos para mas informacion</p>
-            <Button>Contactanos</Button>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/imagen3.png"
-              alt="Picture of the author"
-              width={500}
-              height={500}
-            />
-            <h2 className="text-2xl font-bold">Contacto</h2>
-            <p className="text-lg">Contactanos para mas informacion</p>
-            <Button>Contactanos</Button>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src="/imagen4.png"
-              alt="Picture of the author"
-              width={500}
-              height={500}
-            />
-            <h2 className="text-2xl font-bold">Contacto</h2>
-            <p className="text-lg">Contactanos para mas informacion</p>
-            <Button>Contactanos</Button>
-          </div>
+        <Card img="/imagen1.png" />
+        <Card img="/imagen2.png" />
+        <Card img="/imagen3.png" />
+        <Card img="/imagen4.png" />
       </div>
     </main>
   );
